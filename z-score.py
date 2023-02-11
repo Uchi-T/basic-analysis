e4f6aa299d2e742eab627c2cb25d87721bc0ef86
T.Uchida <99493230+Uchi-T@users.noreply.github.com>
2023-02-12 04:07:47 +0900

    Update z-score.py

--- z-score.py
+++ z-score.py
@@ -1,18 +1,17 @@
-#row z-score を求める。
-#RNA-seq解析の遺伝子発現量を正規化する目的で、1列目が遺伝子名、1行目が実験条件等になっているタブ区切りファイルを使うことを想定。
-#コマンドラインから、"python3 z-score.py ファイル名 インデックスにする列の名前(geneIDなど)"と入力することで実行。
+# row z-score を求める。
+# 例えば、RNA-seq解析の遺伝子発現量を正規化する目的で使う。
+# 上記の例の場合、インプットファイルは、1列目が遺伝子名、2列目以降が発現量、1行目がヘッダになっているタブ区切りテキストファイルを想定。
 
-import scipy as sp #科学技術系の解析に強いライブラリ。
-import pandas as pd #データ解析用ライブラリ。表形式に強い。
+# コマンドラインから、"python3 z-score.py ファイル名"と入力することで実行。
+
+import scipy as sp
+import pandas as pd
 import sys
 
-file = sys.argv[1] #sys.argvはコマンドライン引数のリスト。sys.argv[1]は上記の"ファイル名"にあたる。
-index = sys.argv[2]
+file = sys.argv[1] #上記の"ファイル名"。
 
-df = pd.read_table(file, index_col = index) #データフレームを作成。
+df = pd.read_table(file, index_col = 0) #データフレームを作成。
 zs = df.apply(sp.stats.zscore, axis=1) #行ごとに処理するため、apply関数を使用。z-scoreはscipyの関数を使用して計算。
 zs.to_csv('zscore.tsv',sep = "\t") #タブ区切りテキストとして保存。
 
 #z-scoreの定義上、全ての値が同じ場合は計算されない。
-    
-    
